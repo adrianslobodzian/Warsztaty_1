a6f5908c34215a6ca3a2818c41ef960bc860b3cf
Adrian Słobodzian <adrian.slobodzian@gmail.com>
2017-11-10 16:58:01 +0100

    Game Guess number

--- src/com/github/adrianslobodzian/GameGuess/GuessTheNumber.java
+++ src/com/github/adrianslobodzian/GameGuess/GuessTheNumber.java
@@ -1,9 +1,30 @@
 package com.github.adrianslobodzian.GameGuess;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class GuessTheNumber {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Random r = new Random();
+		int randomNumber = r.nextInt(101);
+		System.out.print("Zgadnij liczbę z przedziału 1 - 100: ");
+		Scanner scan = new Scanner(System.in);
+		int number = -1;
+		while (number != randomNumber) {
+			while (!scan.hasNextInt()) {
+				scan.next();
+				System.out.println("To nie liczba");
+			}
+			number = scan.nextInt();
+			if (number > randomNumber) {
+				System.out.print("Za dużo!");
+			} else if (number < randomNumber) {
+				System.out.print("Za mało!");
+			}
+		}
+		System.out.println("Brawo!");
 
 	}
 
